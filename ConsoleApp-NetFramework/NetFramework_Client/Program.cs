62acf7a47a986c165229b2f6cf6445caeb8bfa9e
nicholasLkading <47288929+nicholasLkading@users.noreply.github.com>
2019-06-09 19:24:22 -0700

    Organized

---
+++ ConsoleApp-NetFramework/NetFramework_Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClientServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientServer.Client client = new ClientServer.Client(new ConnectionArguments("192.168.0.188", 998, '@', Convert.ToByte(';'), 1024))
+            {
+                debug = new Action<string, int>((o, a) =>
+                {
+                    Console.WriteLine(o);
+                })
+            };
+            var msg = client.Communicate(new ClientMessage("repeat", "hello"));
+            Console.WriteLine(msg.message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp-NetFramework/NetFramework_Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using ClientServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientServer.Server server = new ClientServer.Server(new ConnectionArguments("", 998, '@', Convert.ToByte(';'), 1024))
+            {
+                debug = new Action<string, int>((string e, int sum) =>
+                {
+                    Console.WriteLine(e);
+                })
+            };
+            var cmd1 = new Command();
+            cmd1.operation = "repeat";
+            cmd1.action = new Func<ClientMessage, ServerMessage>((o) =>
+            {
+                Console.WriteLine("XX=" + o.message);
+                return new ServerMessage("xx:" + o.message, true);
+            });
+            server.commands = new System.Collections.Generic.List<Command>() { cmd1 };
+            server.Start();
+            Console.ReadLine();
+        }
+    }
+}
